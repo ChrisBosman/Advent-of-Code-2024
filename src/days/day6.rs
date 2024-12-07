0df1b06aa56df560da618bcea29292da07819266
Chris <86408925+ChrisBosman@users.noreply.github.com>
2024-12-07 11:45:03 +0100

    Finished day 7

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,7 +1,98 @@
 use colored::Colorize; 
  
+ 
+#[derive(Default)]
+pub enum Direction {
+    #[default] Up,
+    Left,
+    Down,
+    Right
+}
+
 pub(crate) 
 fn run(input: String) -> (usize, usize){ 
     println!("{}","Day 6".bright_green().bold()); 
-    return (0, 0); 
+    // parse data
+    let mut current_pos = (0, 0, Direction::Up);
+    let is_obstacle = input.lines().enumerate().map(|(i, line)| line.chars().enumerate().map(|(j,char1)| match char1 {
+        '#' => {true},
+        '.' => {false},
+        '^' => {current_pos = (i, j, Direction::Up); false},
+        ch => {panic!("Unknown char \"{}\"", ch)},
+    }).collect::<Vec<bool>>()).collect::<Vec<Vec<bool>>>();
+
+    //* Part 1
+    // Follow the guard until he is out of bounds
+    let mut has_visited = vec![vec![false; is_obstacle[0].len()]; is_obstacle.len()];
+    has_visited[current_pos.0][current_pos.1] = true;
+    'outer: while current_pos.0 < is_obstacle.len() && current_pos.1 < is_obstacle[0].len() && current_pos.0 >= 0 && current_pos.1 >= 0 {
+        // Scan till the next obstacle
+        if let Some(mut next_index) = current_pos.2.get_next_index(&(current_pos.0, current_pos.1), is_obstacle.len() - 1) {
+            while !is_obstacle[next_index.0][next_index.1] {
+                current_pos.0 = next_index.0;
+                current_pos.1 = next_index.1;
+                has_visited[current_pos.0][current_pos.1] = true;
+                if let Some(next) = current_pos.2.get_next_index(&(current_pos.0, current_pos.1), is_obstacle.len() - 1) {
+                    next_index.0 = next.0;
+                    next_index.1 = next.1;
+                } else { break 'outer; }
+            }
+        } else { break; }
+        // There is an obstacle, turn right
+        current_pos.2.turn_right();
+    }
+    let part1 = has_visited.iter().flatten().fold(0, |acc, &x| if x {acc + 1} else {acc});
+    return (part1, 0); 
 } 
+
+fn print_logic_matrix(matrix: &Vec<Vec<bool>>) {
+    for line in matrix {
+        for ch in line {
+            if *ch {print!("#")} else {print!(".")};
+        }
+        println!();
+    }
+}
+
+impl Direction {
+    pub fn turn_left(&mut self) {
+        match *self {
+            Direction::Up => { *self = Direction::Left; },
+            Direction::Left => { *self = Direction::Down; },
+            Direction::Down => { *self = Direction::Right; },
+            Direction::Right => { *self = Direction::Up; },
+        }
+    }
+    pub fn turn_right(&mut self) {
+        match *self {
+            Direction::Up => { *self = Direction::Right; },
+            Direction::Right => { *self = Direction::Down; },
+            Direction::Down => { *self = Direction::Left; },
+            Direction::Left => { *self = Direction::Up; },
+        }
+    }
+    fn get_next_index(&self, current_pos: &(usize, usize), upper_bound: usize) -> Option<(usize, usize)> {
+        match *self {
+            Direction::Up => { Some((current_pos.0.checked_sub(1)?, current_pos.1)) },
+            Direction::Left => { Some((current_pos.0, current_pos.1.checked_sub(1)?)) },
+            Direction::Down => { let next = current_pos.0 + 1; if next > upper_bound {None} else { Some((next, current_pos.1)) }},
+            Direction::Right => { let next = current_pos.1 + 1; if next > upper_bound {None} else { Some((current_pos.0, current_pos.1 + 1)) }},
+        }    
+    }
+    fn turn_right_no_overwrite(&mut self) -> Direction{
+        match *self {
+            Direction::Up => { Direction::Right },
+            Direction::Right => { Direction::Down },
+            Direction::Down => { Direction::Left },
+            Direction::Left => { Direction::Up },
+        }
+    }
+    fn turn_180_no_overwrite(&mut self) -> Direction{
+        match *self {
+            Direction::Up => { Direction::Down },
+            Direction::Right => { Direction::Left },
+            Direction::Down => { Direction::Up },
+            Direction::Left => { Direction::Right },
+        }
+    }
+}
--- src/days/day7.rs
+++ src/days/day7.rs
@@ -3,5 +3,83 @@ use colored::Colorize;
 pub(crate) 
 fn run(input: String) -> (usize, usize){ 
     println!("{}","Day 7".bright_green().bold()); 
-    return (0, 0); 
+    let (test_vals, equations) = parse_input(input);
+    //* Part 1   
+    // See which equations are true when either + or * is inserted between the numbers
+    let mut part1 = 0;
+    let mut part2 = 0;
+    for (answer, equation) in test_vals.iter().zip(equations.iter()){
+        if check_eq(answer, equation) {
+            part1 += answer;
+        }
+        if check_eq_part2(answer, equation) {
+            part2 += answer;
+        }
+    }
+    // println!("Was equation correct: {}", check_eq(&190, &equations[0]));
+    return (part1, part2); 
+}
+
+fn parse_input(input: String) -> (Vec<usize>, Vec<Vec<usize>>) {
+    let mut test_vals = vec![];
+    let mut equations = vec![];
+    let lines = input.lines().map(|line| line.split(": ").collect::<Vec<&str>>());
+    for line in lines{
+        let mut vec1 = vec![];
+        test_vals.push(line[0].parse::<usize>().expect("Could not parse to usize"));
+        let mut parts = line[1].split(" ").map(|str1| str1.parse::<usize>().expect("Could not parse to usize"));
+        while let Some(part) = parts.next() {
+            vec1.push(part);
+        }
+        equations.push(vec1);
+    }
+    (test_vals, equations)
 } 
+
+fn check_eq(answer: &usize, equation: &Vec<usize>) -> bool {
+    if equation.is_empty() { return false;}
+    let max_nr_spots = equation.len()-1;
+    // Max number of options
+    let max_num = f32::exp2(max_nr_spots as f32) as usize;
+    for option in 0..max_num{
+        let mut result = equation[0];
+        // Convert option to binary
+        let bin = (0..max_nr_spots).map(|i| (option & (1 << i)) != 0).collect::<Vec<bool>>();
+        for (&num, &b) in equation.iter().skip(1).zip(bin.iter()) {
+            result = if b {result + num} else {result * num};
+        }
+        if result == *answer {return true;}
+    }
+    return false;
+}
+
+fn check_eq_part2(answer: &usize, equation: &Vec<usize>) -> bool {
+    if equation.is_empty() { return false;}
+    let max_nr_spots = equation.len()-1;    
+    // Max number of options
+    let max_num = f32::powi(3_f32,max_nr_spots as i32) as usize;
+    for option in 0..max_num{
+        let mut result = equation[0];
+        // Convert to base 3
+        let tri = to_base_3(option, max_nr_spots);
+        for (&num, &tri) in equation.iter().skip(1).zip(tri.iter()) {
+            result = match tri {
+                0 => result + num,
+                1 => result * num,
+                2 => (result.to_string() + &num.to_string()).parse().expect("[check_eq_part2] Could not parse to usize"),
+                _ => {println!("Something went wrong in conversion to base 3"); return false;},
+            }
+        }
+        if result == *answer {return true;}
+    }
+    return false;
+}
+
+pub fn to_base_3(mut num: usize, length: usize) -> Vec<usize> {
+    let mut base_3 = vec![0; length];
+    for i in 0..length {
+        base_3[i] = num % 3;
+        num /= 3;
+    }
+    base_3
+}
